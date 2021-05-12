1c6761fe5121a6f6392c437a0cd4d23318988452
Anshu4343 <82087566+Anshu4343@users.noreply.github.com>
2021-05-12 21:09:41 +0530

    Update Wifite.py

--- Wifite.py
+++ Wifite.py
@@ -1,5 +1,7 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
-from wifite import wifite
+# Note: This script runs Wifite from within a cloned git repo.
+# The script `bin/wifite` is designed to be run after installing (from /usr/sbin), not from the cwd.
 
-wifite.run()
+from wifite import __main__
+__main__.entry_point()
